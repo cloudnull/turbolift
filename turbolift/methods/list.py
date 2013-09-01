c510024aa4ff7d89d2d6ee1e7926a939ea5f4119
cloudnull <kevin.carter@rackspace.com>
2013-09-01 00:27:35 -0500

    added new method for simple list of all objects in a container.

---
+++ turbolift/methods/list.py
@@ -0,0 +1,61 @@
+# =============================================================================
+# Copyright [2013] [kevin]
+# License Information :
+# This software has no warranty, it is provided 'as is'. It is your
+# responsibility to validate the behavior of the routines and its accuracy
+# using the code provided. Consult the GNU General Public license for further
+# details (see GNU General Public License).
+# http://www.gnu.org/licenses/gpl.html
+# =============================================================================
+import os
+import sys
+
+import turbolift as clds
+from turbolift import methods
+from turbolift import utils
+from turbolift.clouderator import actions
+from turbolift.worker import ARGS
+from turbolift.worker import LOG
+
+
+class list(object):
+    """Setup and run the list Method."""
+
+    def __init__(self, auth):
+        self.auth = auth
+        self.go = None
+        self.action = None
+
+    def start(self):
+        """Retrieve a long list of all files in a container."""
+
+        # Package up the Payload
+        payload = utils.prep_payload(
+            auth=self.auth,
+            container=ARGS.get('container'),
+            source=None,
+            args=ARGS
+        )
+        self.go = actions.cloud_actions(payload=payload)
+        self.action = getattr(self.go, 'object_lister')
+
+        if ARGS.get('verbose'):
+            LOG.info(
+                'Accessing API for a list of Objects in %s', payload['c_name']
+            )
+
+        if ARGS.get('debug'):
+            LOG.debug('PAYLOAD\t: "%s"', payload)
+
+        with methods.spinner():
+            objects = self.action(url=payload['url'],
+                                  container=payload['c_name'])
+
+        if object is not False:
+            count = len(objects)
+            for obj in objects:
+                print(
+                    'size: %s (bytes) - name: %s '
+                    % (obj.get('bytes'), obj.get('name'))
+                )
+            print('You have %s objects in the container' % count)
